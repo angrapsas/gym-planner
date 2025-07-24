8efe7b42ab1cc553a9b9b73a1905962034ef119b
alexgrapsas <angrapsas@wm.edu>
2025-07-23 19:32:01 -0500

    Remove debug panel

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -1,12 +1,8 @@
 import { CalendarView } from "@/components/calendar-view"
-import { DebugPanel } from "@/components/debug-panel"
-import { DataSeeder } from "@/components/data-seeder"
 
 export default function CalendarPage() {
   return (
     <div className="min-h-screen">
-      <DataSeeder />
-      <DebugPanel />
       <CalendarView />
     </div>
   )
